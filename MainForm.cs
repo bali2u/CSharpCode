be53af9a77b3df7e32f57b60e776ba7dcd5c8898
CodePorting <mohsan.raza@codeporting.com>
2012-07-25 13:01:31 +0500

    update

--- MainForm.cs
+++ MainForm.cs
@@ -22,6 +22,7 @@ namespace MultiFaceRec
         //Declararation of all variables, vectors and haarcascades
         Image<Bgr, Byte> currentFrame;
 		Image<Bgr, Byte> currentFrame1;
+		Image<Bgr, Byte> currentFrame2;
 
         Capture grabber;
         HaarCascade face;
